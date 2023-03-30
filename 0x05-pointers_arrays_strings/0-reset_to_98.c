2a3024581453fdd1efa18be4dd0fb74456306b71
ddanita <daniecodes@gmail.com>
2023-03-30 09:30:20 -0700

    pointer zero task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function that takes a pointer to an int as parameter and updates
+ * reset_to_98 - function that takes a pointer to an int as parameter
  * @n: the value to be updated to 98
  * Return: Always 0
  */
